58d4e60a1916bc0786cf61ab7e1be2ab124950ca
Markberyl <musangaberyl@gmailcom>
2023-03-24 12:28:44 -0700

    configuration

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
-# install puppet-lint -v 2.5.0
-
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+# Define package resource for Flask installation
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
+
+# Refresh the package list after installation
+exec { 'update_package_list':
+  command => '/usr/bin/apt-get update',
+  path    => '/usr/bin:/usr/sbin:/bin',
+  require => Package['flask'],
+}
+
